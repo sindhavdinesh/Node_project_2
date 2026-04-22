eff463e2c0175fa1bd734638e98b2d0cd238236e
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-04-22 19:25:12 +0530

    Refine comments in placeOrder function
    Updated comments in the placeOrder function for clarity.

--- Ecommerce/controllers/order.controller.js
+++ Ecommerce/controllers/order.controller.js
@@ -5,14 +5,14 @@ exports.placeOrder = async (req, res) => {
     try {
         const userId = req.user._id;
 
-        // 1. User ka Cart dhundiye
+        // 1. User 
         const cartItems = await Cart.find({ userId }).populate("productId");
 
         if (cartItems.length === 0) {
             return res.status(400).json({ message: "Cart is empty" });
         }
 
-        // 2. Total Amount calculate karein
+        // 2. Total Amount 
         let totalAmount = 0;
         const orderItems = cartItems.map(item => {
             totalAmount += item.productId.price * item.quantity;
@@ -23,14 +23,14 @@ exports.placeOrder = async (req, res) => {
             };
         });
 
-        // 3. Order create karein
+        // 3. Order create 
         const newOrder = await Order.create({
             userId,
             items: orderItems,
             totalAmount
         });
 
-        // 4. Order hone ke baad Cart khali karein
+        // 4. Order 
         await Cart.deleteMany({ userId });
 
         res.status(201).json({ message: "Order placed successfully", order: newOrder });
