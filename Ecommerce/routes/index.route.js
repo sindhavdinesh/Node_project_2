8c27847eee2a09e81ed2e40354a280d394d45882
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-04-22 10:49:45 +0530

    Reorganize imports and URL mappings in routes

--- Ecommerce/routes/index.route.js
+++ Ecommerce/routes/index.route.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-// 1. Saare Imports Ek Saath (Top par)
+
 const userRouter = require("./user.route");
 const productRoute = require("./product.route");
 const categoryRoute = require("./category.route");
 const cartRoute = require("./cart.route");
-const wishlistRoute = require("./wishlist.route"); // Wishlist import
-const orderRoute = require("./order.route");       // Order import
+const wishlistRoute = require("./wishlist.route"); 
+const orderRoute = require("./order.route");       
 
-// 2. Saare URL Mappings (Bottom par)
+
 router.use("/users", userRouter);
 router.use("/products", productRoute);
 router.use("/categories", categoryRoute);
 router.use("/cart", cartRoute);
-router.use("/wishlist", wishlistRoute); // http://localhost:4000/api/wishlist
-router.use("/orders", orderRoute);      // http://localhost:4000/api/orders
+router.use("/wishlist", wishlistRoute); 
+router.use("/orders", orderRoute);      
 
 module.exports = router;
